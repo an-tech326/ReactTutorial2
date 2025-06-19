6144ee187bd63b7a7905980e745c96287774a327
Libro <libro.1115@gmail.com>
2025-06-19 10:40:55 +0900

    コンポーネント

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 export const App = ()=>{
+    const onClickButton = ()=>alert();
+    const contentStyle = {
+        color:"blue",
+        fontSize:"18px"
+    };
     return (
     <>
-        <h1>hello</h1>
-        <h1>world</h1>
+        <h1 style={{color:"red"}}>hello</h1>
+        <h1 style={contentStyle}>world</h1>
+        <button onClick={onClickButton}>button</button>
     </>
     );
 }
