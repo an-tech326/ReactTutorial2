d0e120ae201cc27b4da5a57a118014b56f082065
Libro <libro.1115@gmail.com>
2025-06-20 17:28:05 +0900

    レンダリング最適化

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useState ,useCallback} from "react";
 import { ChildArea } from "./ChildArea";
 
 export const App = ()=>{
     const [count,setCount]=useState(0);
     const [text, setText]=useState('')
     const [open, setOpen]=useState(false);
-    const onClickCountUp = ()=>setCount(count+1);
     const onChangeText = (e)=>{
         setText(e.target.value);
     }
     const onClickOpen=()=>setOpen(!open);
+    const onClickClose= useCallback(()=>setOpen(false),[setOpen]);
     return (
     <>
        <div className="App">
@@ -17,7 +17,7 @@ export const App = ()=>{
             <br />
             <br />
             <button onClick={onClickOpen}>表示</button>
-            <ChildArea open={open}/>
+            <ChildArea open={open} onClickClose={onClickClose}/>
         </div> 
     </>
     );
