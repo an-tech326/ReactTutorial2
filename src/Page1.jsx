5708a32ad1e6a61ca2d7cb7c25f2194b293ed3fc
Libro <libro.1115@gmail.com>
2025-06-24 17:46:30 +0900

    Navigate

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,14 +1,17 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import { DetailA } from "./Page1DetailA";
 import { DetailB } from "./Page1DetailB";
 export const Page1 = () =>{
     const state = [...Array(10).keys()];
+    const history = useNavigate();
+    const onClickButton = ()=>{history("/page1/detailA",{state:"useButton"})}
     return (
         <>
             <div>
                 <h1>Page1</h1>
-<Link to="DetailA" state={state}>DetailA</Link><br />
-                <Link to="DetailB">DetailB</Link>
+                <Link to="DetailA" state={state}>DetailA</Link><br />
+                <Link to="DetailB">DetailB</Link><br />
+                <button onClick={onClickButton}>MoveDetailA</button>
             </div>
             <Outlet />
         </>
