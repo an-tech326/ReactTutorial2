739bc376669788b0d707bd93281478ae8df0ab36
Libro <libro.1115@gmail.com>
2025-06-25 11:24:42 +0900

    molecules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import { PrimaryButton } from "./atoms/button/PrimaryButton";
 import { SecondaryButton } from "./atoms/button/SecondaryButton";
+import { SearchInput } from "./molecules/SearchInput";
 import "./styles.css";
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <h2>Start editing to see some magic happen!</h2>
       <PrimaryButton>Primary</PrimaryButton>
       <SecondaryButton>Secondary</SecondaryButton>
+      <SearchInput />
     </div>
   );
 }
