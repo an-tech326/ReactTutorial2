7ba25403490f75e54b003a3746c34639d79debd6
Libro <libro.1115@gmail.com>
2025-06-26 14:51:34 +0900

    基本型

---
+++ src/typeList.ts
@@ -0,0 +1,15 @@
+
+let bool:boolean=true;
+let num:number = 0;
+let str:string="";
+let arr1:Array<number>=[];
+let arr2:number[]=[];
+let tuple:[number,boolean]=[0,false];
+let any:any = false;any = 100;
+const funcA=():void=>{
+    const test:string="test";
+}
+let null1:null=null;
+let undefined1=undefined;
+let obj:object={};
+export const dummy=()=>{}
