82bf1a2c7611db04297351974b0d47256dd90916
Libro <libro.1115@gmail.com>
2025-06-26 15:57:51 +0900

    axios API

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 
+import axios from "axios";
 import { Practice1 } from "./practice/practice1";
 import { Practice2 } from "./practice/Practice2";
 import "./styles.css";
 
 export default function App() {
   const onClickFetchData=()=>{
-    axios
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res)=>{
+      console.log(res)
+    })
   }
   return (
     <div className="App">
